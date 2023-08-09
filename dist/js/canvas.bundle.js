1f65f695172bd874e99756b3972947cf5573b469
Milena Neves <80131766+milenafbn@users.noreply.github.com>
2023-08-09 01:01:20 -0300

    Adição de imagens nas plataformas

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -86,6 +86,19 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/img/myplatform.png":
+/*!********************************!*\
+  !*** ./src/img/myplatform.png ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "68ded96849c5f086e582ad5fbbd2a9db.png");
+
+/***/ }),
+
 /***/ "./src/js/canvas.js":
 /*!**************************!*\
   !*** ./src/js/canvas.js ***!
@@ -95,8 +108,7 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _img_myplatform_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../img/myplatform.png */ "./src/img/myplatform.png");
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -104,105 +116,211 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 
+console.log(_img_myplatform_png__WEBPACK_IMPORTED_MODULE_0__["default"]);
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-var mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
-};
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']; // Event Listeners
+canvas.width = 1024; //tag canvas takes up the full width
 
-addEventListener('mousemove', function (event) {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
-addEventListener('resize', function () {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-  init();
-}); // Objects
+canvas.height = 576; //create a player with a class named player
 
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
+var gravity = 0.5;
 
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-  }
+var Player = /*#__PURE__*/function () {
+  function Player() {
+    _classCallCheck(this, Player);
 
-  _createClass(Object, [{
+    //propriedades do jogador. Aqui ele vai ser um quadrado
+    this.position = {
+      x: 100,
+      y: 100
+    };
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
+    this.width = 30;
+    this.height = 30;
+  } //method draw: desenhando o personagem
+
+
+  _createClass(Player, [{
     key: "draw",
     value: function draw() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-      c.closePath();
+      c.fillStyle = 'blue';
+      c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
   }, {
     key: "update",
     value: function update() {
       this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+      if (this.position.y + this.height + this.velocity.y <= canvas.height) this.velocity.y += gravity;else this.velocity.y = 0;
     }
   }]);
 
-  return Object;
-}(); // Implementation
+  return Player;
+}();
 
+var Platform = /*#__PURE__*/function () {
+  function Platform(_ref) {
+    var x = _ref.x,
+        y = _ref.y,
+        image = _ref.image;
 
-var objects;
+    _classCallCheck(this, Platform);
 
-function init() {
-  objects = [];
+    // ({ }) é um objeto, não argumentos tipo x,y apenas
+    this.position = {
+      x: x,
+      // = x: x,
+      y: y // = y: y
 
-  for (var i = 0; i < 400; i++) {// objects.push()
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
   }
-} // Animation Loop
 
+  _createClass(Platform, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+
+  return Platform;
+}();
+
+var image = new Image();
+image.src = _img_myplatform_png__WEBPACK_IMPORTED_MODULE_0__["default"];
+console.log(image);
+var player = new Player();
+var platforms = [new Platform({
+  x: -1,
+  y: 470,
+  image: image
+}), new Platform({
+  x: image.width - 0.9,
+  y: 470,
+  image: image
+})];
+var keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+};
+var scrollOffset = 0; //deslocamento de rolagem
 
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  c.fillStyle = 'white';
+  c.fillRect(0, 0, canvas.width, canvas.height); //limpa toda a tela
+
+  platforms.forEach(function (platform) {
+    platform.draw();
+  });
+  player.update();
+
+  if (keys.right.pressed && player.position.x < 400) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed && player.position.x > 100) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+
+    if (keys.right.pressed) {
+      scrollOffset += 5;
+      platforms.forEach(function (platform) {
+        platform.position.x -= 5;
+      });
+    } else if (keys.left.pressed) {
+      scrollOffset -= 5;
+      platforms.forEach(function (platform) {
+        platform.position.x += 5;
+      });
+    }
+  } //console.log(scrollOffset)
+  //platform collision detection
+
+
+  platforms.forEach(function (platform) {
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+      player.velocity.y = 0;
+    }
+  });
+
+  if (scrollOffset > 2000) {
+    console.log('YOU WIN!');
+  }
 }
 
-init();
 animate();
+addEventListener('keydown', function (_ref2) {
+  var keyCode = _ref2.keyCode;
 
-/***/ }),
+  switch (keyCode) {
+    case 37:
+    case 65:
+      console.log('left');
+      keys.left.pressed = true;
+      break;
 
-/***/ "./src/js/utils.js":
-/*!*************************!*\
-  !*** ./src/js/utils.js ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+    case 40:
+    case 83:
+      console.log('down');
+      break;
 
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+    case 39:
+    case 68:
+      console.log('right');
+      keys.right.pressed = true;
+      break;
 
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
+    case 38:
+    case 87:
+      console.log('up');
+      player.velocity.y -= 1;
+      break;
 
-function distance(x1, y1, x2, y2) {
-  var xDist = x2 - x1;
-  var yDist = y2 - y1;
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-}
+    default:
+      console.log('invalid key');
+  }
+});
+addEventListener('keyup', function (_ref3) {
+  var keyCode = _ref3.keyCode;
 
-module.exports = {
-  randomIntFromRange: randomIntFromRange,
-  randomColor: randomColor,
-  distance: distance
-};
+  switch (keyCode) {
+    case 37:
+    case 65:
+      console.log('left');
+      keys.left.pressed = false;
+      break;
+
+    case 40:
+    case 83:
+      console.log('down');
+      break;
+
+    case 39:
+    case 68:
+      console.log('right');
+      keys.right.pressed = false;
+      break;
+
+    case 38:
+    case 87:
+      console.log('up');
+      player.velocity.y -= 20;
+      break;
+
+    default:
+      console.log('invalid key');
+  }
+});
 
 /***/ })
 
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,185 @@
-import utils from './utils'
+import myplatform from '../img/myplatform.png';
+console.log(myplatform)
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = 1024 //tag canvas takes up the full width
+canvas.height = 576
+//create a player with a class named player
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+const gravity = 0.5
+class Player{
+    constructor(){
+        //propriedades do jogador. Aqui ele vai ser um quadrado
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.width = 30
+        this.height = 30
+    }
+
+    //method draw: desenhando o personagem
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if(this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
+class Platform{
+    constructor({x,y,image}){ // ({ }) é um objeto, não argumentos tipo x,y apenas
+        this.position = {
+            x,  // = x: x,
+            y  // = y: y
+        }
 
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
+        this.image = image
 
-  init()
-})
+        this.width = image.width
+        this.height = image.height
 
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
+    }
 
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
-  }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
+const image = new Image()
+image.src = myplatform
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
+console.log(image)
+
+const player = new Player()
+const platforms = [new Platform({x:-1, y:470, image}), 
+  new Platform({x: image.width - 0.9, y:470, image})]
+
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
 }
 
-// Animation Loop
+let scrollOffset = 0 //deslocamento de rolagem
+
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+    requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height) //limpa toda a tela
+    
+    platforms.forEach((platform) => {
+        platform.draw()
+    })
+    player.update()
+    if (keys.right.pressed && player.position.x < 400){
+        player.velocity.x = 5
+    }else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5
+    }else {
+        player.velocity.x = 0
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
-}
+        if (keys.right.pressed) {
+            scrollOffset += 5
+            platforms.forEach((platform) => {
+                platform.position.x -= 5
+            })
+        } else if (keys.left.pressed) {
+            scrollOffset -= 5
+            platforms.forEach((platform) => {
+                platform.position.x += 5
+            })
+        }
+    }
 
-init()
+    //console.log(scrollOffset)
+
+    //platform collision detection
+    platforms.forEach((platform) => {
+        if (player.position.y + player.height <= platform.position.y 
+        && player.position.y + player.height + player.velocity.y 
+        >= platform.position.y && player.position.x + player.width
+        >= platform.position.x && player.position.x 
+        <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+    })
+
+    if (scrollOffset > 2000) {
+        console.log('YOU WIN!')
+    }
+}           
+
 animate()
+
+addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+        case 37:
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+            break
+        case 40:
+        case 83:
+            console.log('down')
+            break
+        case 39:
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+            break
+        case 38:
+        case 87:
+            console.log('up')
+            player.velocity.y -= 1
+            break
+        default:
+            console.log('invalid key')
+    }
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+        case 37:
+        case 65:
+            console.log('left')
+            keys.left.pressed = false
+            break
+        case 40:
+        case 83:
+            console.log('down')
+            break
+        case 39:
+        case 68:
+            console.log('right')
+            keys.right.pressed = false
+            break
+        case 38:
+        case 87:
+            console.log('up')
+            player.velocity.y -= 20
+            break
+        default:
+            console.log('invalid key')
+    }
+})
